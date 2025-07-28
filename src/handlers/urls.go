e15f3b7de423b5856373706cdff0d2998d2207a2
townsag <townsag@gmail.com>
2025-07-28 00:13:41 -0700

    move add routes out of urls.go

--- src/handlers/urls.go
+++ src/handlers/urls.go
@@ -156,17 +156,3 @@ func redirectToLongUrlHandlerFactory(conn *pgx.Conn) http.HandlerFunc {
 		http.Redirect(w, r, record.LongUrl, http.StatusFound)
 	}
 }
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello world\n")
-}
-
-func AddRoutes(mux *http.ServeMux, conn *pgx.Conn) {
-	// HandleFunc under the hood creates and HandlerFunc object from the hello function and 
-	// assignes that HandlerFunc object to the relevant pattern
-	// The HandlerFunc type is just a function with an ServeHttp method defined on it that calls
-	// the function
-	mux.HandleFunc("/hello", hello)
-	mux.HandleFunc("GET /{shortUrlId}", redirectToLongUrlHandlerFactory(conn))
-	mux.HandleFunc("POST /mapping", createMappingHandlerFactory(conn))
-}
