891095b85cfcefd0a659f3e0ae94bc2458d76c31
townsag <86760109+townsag@users.noreply.github.com>
2025-07-30 19:52:01 -0700

    feat: cache server (#4)
    * add redis container
    
    * add application logic for accessing redis

--- src/main.go
+++ src/main.go
@@ -7,16 +7,18 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
 
 	"townsag/url_shortener/src/middleware"
 	"townsag/url_shortener/src/handlers"
 )
 
-func newServer(conn *pgx.Conn) http.Handler {
+func newServer(conn *pgx.Conn, rdb *redis.Client) http.Handler {
 	mux := http.NewServeMux()
 	handlers.AddRoutes(
 		mux,
 		conn,
+		rdb,
 	)
 
 	root_logger := middleware.BuildLogger()
@@ -38,7 +40,14 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	srv := newServer(conn)
+	var redisConfig *redisConfig = getRedisConfiguration()
+	rdb, err := createRedisConnection(ctx, redisConfig)
+	if err != nil {
+		log.Fatalf("failed to create a redis connection: %s", err)
+	}
+	defer rdb.Close()
+
+	srv := newServer(conn, rdb)
 	httpServer := &http.Server{
 		Addr: net.JoinHostPort("0.0.0.0", "8000"),
 		Handler: srv,
